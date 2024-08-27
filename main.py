e81147dc1bd5e366b83fade1814732350f47c2d6
Christopher Faris <142948338+chrisF943@users.noreply.github.com>
2024-08-27 17:16:58 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import datetime
+import os
+
+stock_url = "https://www.alphavantage.co/query"
+stock_key = "WDK56JX91HXK0CQ1"
+
+tickers = ["IBM", "AAPl", "MSFT", "GOOGL", "ORCL"]
+values = []
+
+for ticker in tickers:
+    params = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": ticker,
+        "apikey": stock_key,
+    }
+
+    response = requests.get(stock_url, params)
+    response.raise_for_status()
+    data = response.json()["Time Series (Daily)"]
+    most_recent_date = max(data.keys())
+    close_value = data[most_recent_date]["4. close"]
+    values.append(f"{ticker}: {float(close_value)}")
+    print(f"{ticker}: {float(close_value)}")
+
+today = datetime.datetime.now().strftime("%m/%d/%Y")
+
+path = "/Users/chrisfaris/Desktop"
+filename = "stocks.txt"
+full_path = os.path.join(path, filename)
+with open(full_path, "a") as file:
+    file.write(f"\n{today}\n")
+    for value in values:
+        file.write(f"{value}\n")
